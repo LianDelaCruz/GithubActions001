959c6eff58bfcec0a60d5f6375b1718111768ba3
Takikato09 <liandelacruz09@hotmail.com>
2025-05-13 16:27:13 +0200

    Changed xml and yml

--- src/main/java/calculator/MyClass.java
+++ src/main/java/calculator/MyClass.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class MyClass {
     public int add(int a, int b) {
-        System.out.println("testing...");
+        System.out.println("TESTING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         return a + b + 1;
     }
 
